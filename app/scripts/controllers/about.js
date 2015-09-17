9fedd7968989a27902d043889e53d7abbbf1a618
unknown <anbestephen@gmail.com>
2015-09-17 19:55:14 -0400

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name collaborationToolApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the collaborationToolApp
+ */
+angular.module('collaborationToolApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name collaborationToolApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the collaborationToolApp
+ */
+angular.module('collaborationToolApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
