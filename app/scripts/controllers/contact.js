5163e28826fb76d509a985d6f5e8292e50938a86
unknown <anbestephen@gmail.com>
2015-09-22 23:05:03 -0400

    Added contact tab

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name collaborationToolApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the collaborationToolApp
+ */
+angular.module('collaborationToolApp')
+  .controller('ContactCtrl', function () {
+    
+  });
